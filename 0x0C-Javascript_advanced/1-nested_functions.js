7961d192dc9e21cd40107e6448c8a5e0916f73a7
saidskander <askander199h@outlook.com>
2021-06-28 09:27:38 +0200

    Done

---
+++ 0x0C-Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,16 @@
+// Scope Chain
+const globalVariable = "Welcome";
+function outer() {
+ alert(globalVariable);
+ const course = "Holberton";
+ const exclamation = "!";
+  function inner() {
+   alert(`${globalVariable} ${course}`);
+   function inception() {
+     alert(`${globalVariable} ${course}${exclamation}`);
+   }
+   inception();
+  }
+  inner();
+}
+outer();
