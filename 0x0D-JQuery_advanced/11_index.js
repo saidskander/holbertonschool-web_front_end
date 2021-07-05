381f816ac7d1f1dc03a02c600e006e1f23f42a0f
saidskander <askander199h@outlook.com>
2021-07-05 11:21:46 +0200

    Done

--- 0x0D-JQuery_advanced/11_index.js
+++ 0x0D-JQuery_advanced/11_index.js
@@ -1,56 +1,57 @@
-// Another Get API -- json
+// Post query
 function addPostRow(data) {
-  pargrph = '<p><span>Post created with id ' + data.id + ', title: ' + data.title + ', author: ' + data.author + '</span></p>';
-  $("body").append(pargrph);
+  prgph = '<p><span>Post created with id ' + ', id: ' + data.id + ', title: ' + data.title + ', author: ' + data.author + '</span></p>';
+  $("body").append(prgph);
 }
 
 function listPosts() {
-  // using atom port 3000
   $.get('http://localhost:3000/posts', function(data) {
-    // length of the data
     let len = data.length;
-    for (let x = 0; x < len; ++x) {
-       addPostRow(data[x]);
+    for (let x = 1; x < length; ++x) {
+      addPostRow(data[x]);
     }
   });
 }
 
 function buildForm() {
-  searchform = '<form action="sendform()">\n<div class="box">\n<label for="by">Author</label>\n<input name="uno" type="text" id="by">\n</div>';
-  searchform += '<div class="box">\n<label for="tit">Title</label>\n<textarea name="dos" id="tit" cols="12" rows="1"></textarea>\n</div>'
-  searchform += '<input type="submit" value="Submit">\n</form>'
-  $( 'body' ).prepend( searchform );
+  html_zone = '<form action="POST">\n<div class="box">\n<label for="by">Author</label>\n<input name="uno" type="text" id="by">\n</div>';
+  html_zone += '<div class="box">\n<label for="tit">Title</label>\n<textarea name="dos" id="tit" cols="12" rows="1"></textarea>\n</div>'
+  html_zone += '<input type="submit" value="Submit">\n</form>'
+  $('body').prepend(html_zone);
   sendForm();
 }
 
 function sendForm() {
-  $( "form" ).submit(function( e ) {
-    $( 'form' ).after( '<p class="info">About to send the query to the API</p>' );
+  $("form").submit(function(e) {
+    $('form').after('<p class="detail">About to send the query to the API</p>');
 
-    let values = $( this ).serializeArray();
+    let values = $(this).serializeArray();
     author = values[0].value;
     title = values[1].value;
+
     add_new_article = {
       title: "",
       author: "",
     }
+
     add_new_article.title = title;
     add_new_article.author = author;
-    $.post( 'http://localhost:3000/posts', add_new_article)
+    $.post('http://localhost:3000/posts', add_new_article)
 
-    .done(function() {
-      addPostRow(add_new_article);
-    })
+      .done(function() {
+        addPostRow(add_new_article);
+      })
 
-    .fail(function () {
-      alert('Error sending the POST query');
-    });
+      .fail(function() {
+        alert('Error sending the POST query');
+      });
 
-    $( '.info' ).remove();
+    $('.detail').remove();
     e.preventDefault();
   });
 }
 
-$( document ).ready(function() {
+$(document).ready(function() {
   listPosts();
+  buildForm();
 });
