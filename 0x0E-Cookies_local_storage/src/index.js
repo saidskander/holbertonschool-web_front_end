0d92cb83960098478cf643d09d4bb99bb23a66b2
saidskander <askander199h@outlook.com>
2021-07-07 16:37:20 +0200

    Done

--- 0x0E-Cookies_local_storage/src/index.js
+++ 0x0E-Cookies_local_storage/src/index.js
@@ -1,1 +1,30 @@
-
+// basic cookie
+function setCookies(e) {
+    const firstname = document.getElementById('firstname').value;
+    const email = document.getElementById('email').value;
+  
+    document.cookie = `${firstname}=${email};`;
+  }
+  
+  function showCookies(e) {
+    const p = document.createElement('p');
+  
+    const decodeCookie = decodeURIComponent(document.cookie);
+    const all = decodeCookie.split(';');
+    let cookies = '<strong>Cookies:</strong> ';
+  
+    for (let x = 0; x < (all.length - 1); ++x) {
+      cookies += ` "${all[x]}"`;
+    }
+  
+    p.innerHTML = cookies;
+    document.querySelector('main').append(p);
+  }
+  
+  document.addEventListener("DOMContentLoaded", function(event) {
+    const formlog = document.getElementById('log');
+    const showcookis = document.getElementById('btn-show-cook');
+  
+    formlog.addEventListener('submit', setCookies);
+    showcookis.addEventListener('click', showCookies);
+  })
